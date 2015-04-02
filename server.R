bf5b77be542d3e2d9d1ece028efc0e13340108ef
DanGarcia595 <Dgarcia1@umassd.edu>
2015-04-02 08:38:00 -0400

    Added the return from the MLE function in GO_BM.R to data in order to get enough points to graph

--- server.R
+++ server.R
@@ -5,66 +5,69 @@ source("model.R")#Source for our reliabilty models
 source("JMmodel.R")
 source("GO_BM.R")
 shinyServer(function(input, output) {
-
+  
   output$distPlot <- renderPlot({ #reactive function, basically Main()
-
-  inFile <- input$file #Read of input file
+    
+    inFile <- input$file #Read of input file
     if (is.null(inFile))#error handling for null file pointer
       return("Please Upload a CSV File")
     if (input$type==1)
       data <- read.xls(inFile$datapath,sheet=1)#Reads xls and xlsx files. Error handling needed
     if (input$type==2)
       data <- read.csv(inFile$datapath, header = input$header, sep = input$sep , quote = " % ")#same as before needs error handling
-
-  #if (data[1] =="FC")
+    
+    #if (data[1] =="FC")
     #two coumns
-  #else
+    #else
     #one column
-
-   Time <- names(data[1])#generic name of column name of data frame (x-axis)
-   Failure <- names(data[2])#(y-axis)
-   p <- ggplot(,aes_string(x=Time,y=Failure))#This function needs aes_string() to work
-   value <- c("red","blue") 
-   model <- ""
-   if (input$OD == TRUE){
-   p <- p + geom_point(data = data,aes(color="blue",group="Original Data")) + geom_line(data = data,aes(color="blue",group="Original Data"))#adds scatter plot points to plot object
-   label <- c("Original Data","")
-   value <- c("blue","red")
-   }
-   p <- p + scale_color_manual(name = "Legend",  labels = c("Original Data"),values = c("blue"))
-   if (input$Model == "JM"){
+    
+    Time <- names(data[1])#generic name of column name of data frame (x-axis)
+    Failure <- names(data[2])#(y-axis)
+    p <- ggplot(,aes_string(x=Time,y=Failure))#This function needs aes_string() to work
+    value <- c("red","blue") 
+    model <- ""
+    if (input$OD == TRUE){
+      p <- p + geom_point(data = data,aes(color="blue",group="Original Data")) + geom_line(data = data,aes(color="blue",group="Original Data"))#adds scatter plot points to plot object
+      label <- c("Original Data","")
+      value <- c("blue","red")
+    }
+    p <- p + scale_color_manual(name = "Legend",  labels = c("Original Data"),values = c("blue"))
+    if (input$Model == "JM"){
       newdata <- JMmodel(data)
       p <- p + geom_point(data=newdata,aes(color="red",group="Jolinski-Moranda Model"))
       model <- c("Jolinski-Moranda Model")
-        }
-   if (input$Model == "GEO"){
+    }
+    if (input$Model == "GEO"){
       newdata <- GeoModel(data)
       p <- p + geom_point(data=newdata,aes(color="red",group="Geometric Model"))
       p <- p + geom_line(data=newdata,aes(color="red",group="Geometric Model"))
       model <- c("Geometric Model")
-        }
-  if (input$Model == "GO"){
-      newdata <- GO_BM_MLE(data)
+    }
+    if (input$Model == "GO"){
+      y <- as.vector(data[,2])
+      print(y)
+      newdata <- GO_BM_MLE(y)
       print(newdata)
+      newdata <- data + newdata
       p <- p + geom_point(data=newdata,aes(color="red",group="Geol-Okumoto Model"))
       p <- p + geom_line(data=newdata,aes(color="red",group="Geol-Okumoto Model"))
       model <- c("Geol-Okumoto Model")
-        }
-  if (input$Model == "YS"){
+    }
+    if (input$Model == "YS"){
       newdata <- YamadaModel(data)
       p <- p + geom_point(data=newdata,aes(color="red",group="Yamada S-Shaped Model"))
       p <- p + geom_line(data=newdata,aes(color="red",group="Yamada S-Shaped Model"))
       model <- c("Yamada S-Shaped Model")
-   }
-   if(input$OD == FALSE){
-   label = c(model,"")
-   }else{
-   label = c("Original Data",model)
-   }
+    }
+    if(input$OD == FALSE){
+      label = c(model,"")
+    }else{
+      label = c("Original Data",model)
+    }
     p <- p + scale_color_manual(name = "Legend",  labels = label,values = value)
-
-   p
- #plot(data) Leave this here to use if ggplot() stops working. 
- } )
+    
+    p
+    #plot(data) Leave this here to use if ggplot() stops working. 
+  } )
 })
 
