0a544bef9d17314165b32539a881432fa2e79f5c
arhik <kkatipally@umassd.edu>
2015-06-22 10:34:50 -0400

    Fixed bug in FT to IF conersion

--- Data_Format.R
+++ Data_Format.R
@@ -1,87 +1,59 @@
 #The purpose of these fuctions are to convert between failure times and interfailure times
-failureT_to_interF <- function(interfailure)
+failureT_to_interF <- function(failure_T)
 {
-
-failure_T <- c()
-failure_T[1] = interfailure[1] #initial value for failure time
-
-n = 2
-
-while(n<=length(interfailure))
-{
-   failure_T[n] = interfailure[n] - interfailure[n-1]
-   
-   n = n+1
-}
-
-return(failure_T)#return failure times(failure_T)
-}
-
-
-
-
-#################################
-
-
-
- interF_to_failureT <- function(failure_T)
-{
-
 
 interfailure <- c()
-
 interfailure[1] = failure_T[1] #initial value for failure time
 
 n = 2
 
 while(n<=length(failure_T))
 {
-  interfailure[n] = failure_T[n] + interfailure[n-1]
-  
-  n = n + 1
+  interfailure[n] = failure_T[n] - failure_T[n-1]
+   
+   n = n+1
+}
+
+return(interfailure)#return failure times(failure_T)
 }
 
 
-return(interfailure)#return interfailure times(interfailure)
-}
 
 
-
-CumulativeFailureC_to_failureC <- function(x){
-  fc <- c()
-  x <- c(0,x)
-  for(i in 1:(length(x)-1)){
-     fc[i] <- x[i+1] - x[i]   
-  }
-  return(fc)
-}
 
 #################################
 
 
 
+ interF_to_failureT <- function(interfailure) #interfailure to failure times
+{
 
-#failureC_to_failureT <- function(failure_Count)
-#{
 
-#failure_T <- c()
+   failure_T <- c()
 
-#n = 1
-#i = 1
+   failure_T[1] = interfailure[1] #initial value for failure time
 
-#while(n<=length(failure_Count))
-#{
-#  if(failure_Count[n] != 0)
-#  {
-#    failure_T[i] = n
-#    i = i + 1
- # }
+n = 2
+
+while(n<=length(interfailure))
+{
+  failure_T[n] = interfailure[n] + failure_T[n-1]
   
- # n = n + 1;
-#}
+  n = n + 1
+}
 
-#return(failure_T)#return failure times(failure_t)
-#}
+
+return(failure_T)#return interfailure times(interfailure)
+}
+
+
+
+
+
+#################################
+
+
+
 
 failureC_to_failureT <- function(time_vec,num_count) #failure count to failure time
 {
@@ -102,3 +74,60 @@ failureC_to_failureT <- function(time_vec,num_count) #failure count to failure t
   return(failure_T)
 }
 
+CumulativeFailureC_to_failureC <- function(x){
+  fc <- c()
+  x <- c(0,x)
+  for(i in 1:(length(x)-1)){
+     fc[i] <- x[i+1] - x[i]   
+  }
+  return(fc)
+}
+
+
+
+
+
+
+createFields <- function(data){
+  len <- nrow(data[1]) #length of data
+  FC<-c(1:len)  #vector of failure counts from 1 to length
+  names(FC)<-"FC" #naming the vector
+  
+  if (names(data[1]) =="FT"){ #if the first column is failure times, convert to interfail
+    FT <- data[,1]
+    names(FT)<-"FT"
+    IF <- failureT_to_interF(data[,1]) #converts from failure times to interfailure times
+    names(IF)<-"IF"
+  }else if(names(data[1]) == "IF"){ #if the first column is interfailure times, convert to failure time
+    IF <- data[,1]
+    names(IF)<-"IF"
+    FT <-interF_to_failureT(data[,1]) 
+    names(FT)<-"FT"
+  }else if(names(data[1]) == "FC") { #if the first column is failure count and next rows are IF or FT
+    FC <- data[,1]
+    names(FC)<-"FC"
+    if(names(data[2])=="FT"){#if second row is failure time find IF
+      FT <- data[,2]
+      names(FT)<-"FT"
+      IF <- failureT_to_interF(data[,2])
+      names(IF)<-"IF"
+    }else if(names(data[2])=="IF"){#if second row is interfailure times find FT
+      IF <- data[,2]
+      names(IF)<-"IF"
+      FT <-interF_to_failureT(data[,2])
+      names(FT)<-"FT"}
+  }
+  return(list(FC=FC,FT=FT,IF=IF))
+}
+
+failureN_to_failureCount <-function(x)
+{
+  #print("HI i am X")
+  print(x)  
+  bins <- seq(min(x), max(x), length.out = length(x))
+  histo <- hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  r <- data.frame(histo$counts)
+  print(r)
+  r$x
+
+}
--- GM_BM.R
+++ GM_BM.R
@@ -1,14 +1,16 @@
-x <- c(3, 33, 146, 227, 342, 351, 353, 444,556, 571, 709, 759, 836, 860, 968,1056, 1726, 1846, 1872, 1986, 2311,2366, 2608, 2676, 3098, 3278, 3288,4434, 5034, 5049, 5085, 5089, 5089,5097, 5324, 5389, 5565, 5623, 6080,6380, 6477, 6740, 7192, 7447, 7644,7837, 7843, 7922, 8738, 10089, 10237,10258, 10491, 10625, 10982, 11175,11411, 11442, 11811, 12559, 12559,12791, 13121, 13486, 14708, 15251,15261, 15277, 15806, 16185, 16229,16358, 17168, 17458, 17758, 18287,18568, 18728, 19556, 20567, 21012,21308, 23063, 24127, 25910, 26770,27753, 28460, 28493, 29361, 30085,32408, 35338, 36799, 37642, 37654,37915, 39715, 40580, 42015, 42045,42188, 42296, 42296, 45406, 46653,47596, 48296, 49171, 49416, 50145,52042, 52489, 52875, 53321, 53443,54433, 55381, 56463, 56485, 56560, 57042, 62551, 62651, 62661, 63732,64103, 64893, 71043, 74364, 75409,76057, 81542, 82702, 84566, 88682)
+#x <- c(3, 33, 146, 227, 342, 351, 353, 444,556, 571, 709, 759, 836, 860, 968,1056, 1726, 1846, 1872, 1986, 2311,2366, 2608, 2676, 3098, 3278, 3288,4434, 5034, 5049, 5085, 5089, 5089,5097, 5324, 5389, 5565, 5623, 6080,6380, 6477, 6740, 7192, 7447, 7644,7837, 7843, 7922, 8738, 10089, 10237,10258, 10491, 10625, 10982, 11175,11411, 11442, 11811, 12559, 12559,12791, 13121, 13486, 14708, 15251,15261, 15277, 15806, 16185, 16229,16358, 17168, 17458, 17758, 18287,18568, 18728, 19556, 20567, 21012,21308, 23063, 24127, 25910, 26770,27753, 28460, 28493, 29361, 30085,32408, 35338, 36799, 37642, 37654,37915, 39715, 40580, 42015, 42045,42188, 42296, 42296, 45406, 46653,47596, 48296, 49171, 49416, 50145,52042, 52489, 52875, 53321, 53443,54433, 55381, 56463, 56485, 56560, 57042, 62551, 62651, 62661, 63732,64103, 64893, 71043, 74364, 75409,76057, 81542, 82702, 84566, 88682)
 
-n=length(x)
 
-interFail <- c(NULL)
+GM_BM_MLE <- function(interFail){
+n=length(interFail)
 
-for(i in 2:n){
-  interFail[i-1]=x[i]-x[i-1]
-}
+# interFail <- c(NULL)
 
-interFail <- c(x[1], interFail)
+# for(i in 2:n){
+#   interFail[i-1]=x[i]-x[i-1]
+# }
+
+# interFail <- c(x[1], interFail)
 
 #Define MLE of parameter 'phi'
 MLEeq<-function(phi){
@@ -33,19 +35,19 @@ b0 <- 1.0
 #Step-2: Bracket root
 
 i <- 0 
-maxIterations <- 10
+maxIterations <- 20
 leftEndPoint <- b0/2
 leftEndPointMLE <- MLEeq(leftEndPoint)
-rightEndPoint <- b0
+rightEndPoint <- 1.1*b0
 rightEndPointMLE <- MLEeq(rightEndPoint)
 
-while(leftEndPointMLE*rightEndPointMLE > 0 & i <= maxIterations){
-  print('In Step 2 while loop of GM_BM.R')
+while(i <= maxIterations){
+  #print('In Step 2 while loop of GM_BM.R')
   #leftEndPoint <- leftEndPoint/2
   #leftEndPointMLE <- MLEeq(leftEndPoint)
-  rightEndPoint <- rightEndPoint
-  rightEndPointMLE <- MLEeq(rightEndPoint)
-  i <- i+1	
+  #rightEndPoint <- 1.1*rightEndPoint
+  #rightEndPointMLE <- MLEeq(rightEndPoint)
+  i <- i+1  
 }
 
 #Step-3: Invoke uniroot or report non convergence to calling environment
@@ -55,7 +57,7 @@ if(leftEndPointMLE*rightEndPointMLE > 0 ){
 } else {
   phiMLE <- uniroot(MLEeq,lower=leftEndPoint,upper=rightEndPoint, extendInt="yes", tol = 1e-10)$root
 }
-print(phiMLE)
+#print(phiMLE)
 
 #Step-4
 #MLE of parameter 'D'
@@ -65,4 +67,6 @@ for(i in 1:n){
 }
 D_MLE <- (phiMLE*n)/DrTerm
 
-print(D_MLE)
+#print(D_MLE)
+return(c(D_MLE,phiMLE))
+}
