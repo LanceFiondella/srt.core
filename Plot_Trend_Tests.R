d252621b22234cc37b9306a22c5a6d0552271a37
Allen Nikora <Allen.P.Nikora@jpl.nasa.gov>
2015-08-06 20:07:36 -0700

    Subsetting logic implemented to display the trend tests for the data specified by the model data range slider.  The results of all subsetting operations are cast into unnamed flat arrays.

--- Plot_Trend_Tests.R
+++ Plot_Trend_Tests.R
@@ -4,35 +4,44 @@ DataIntervalEnd <- input$modelDataRange[2]
 if((DataIntervalEnd - DataIntervalStart + 1) >= K_minDataModelIntervalWidth) {
   q <- ggplot(,aes_string(x="index",y="trend_test_statistic"))
   if((length(grep("FT",names(input_data)))>0) || (length(grep("IF",names(input_data))) > 0)) {
-    if(length(grep("FT",names(input_data)))>0) {
-      FT <- input_data$FT
-      IF <- failureT_to_interF(input_data$FT)
-    } else if(length(grep("IF",names(input_data)))>0) {
-      FT <- interF_to_failureT(data$IF)
-      IF <- input_data$IF
-    }
+    IF <- c(unlist(subset(subset(input_data, input_data$FN >= DataIntervalStart, select = c(FN, IF, FT)), FN <= DataIntervalEnd, select = IF)), use.names=FALSE)
+    FT <- c(unlist(subset(subset(input_data, input_data$FN >= DataIntervalStart, select = c(FN, IF, FT)), FN <= DataIntervalEnd, select = FT)), use.names=FALSE)
   } else if((length(grep("CFC",names(input_data)))>0) || (length(grep("FC",names(input_data)))>0)) {
-    if (length(grep("CFC",names(input_data)))>0) {
-      CFC <- input_data$CFC[input_data$CFC > 0]
-      FC <- CumulativeFailureC_to_failureC(CFC)
-      T <- input_data$T[input_data$CFC > 0]
-    } else if (length(grep("FC",names(input_data)))>0) {
-      FC <- input_data$FC[input_data$FC > 0]
-      CFC <- FailureC_to_CumulativeFailureC(FC)
-      T <- input_data$T[input_data$CFC > 0]
+    FC <- c(unlist(subset(subset(input_data, input_data$TI >= DataIntervalStart, select = c(TI, T, FC, CFC)), TI <= DataIntervalEnd, select = FC)), use.names=FALSE)
+    CFC <- c(unlist(subset(subset(input_data, input_data$TI >= DataIntervalStart, select = c(TI, T, FC, CFC)), TI <= DataIntervalEnd, select = CFC)), use.names=FALSE)
+    CumT <- c(unlist(subset(subset(input_data, input_data$TI >= DataIntervalStart, select = c(TI, T, FC, CFC)), TI <= DataIntervalEnd, select = T)), use.names=FALSE)
+    TI <- c(unlist(subset(subset(input_data, input_data$TI >= DataIntervalStart, select = c(TI, T, FC, CFC)), TI <= DataIntervalEnd, select = TI)), use.names=FALSE)
+    
+    if(DataIntervalStart > 1) {
+      FT <- c(unlist(failureC_to_failureT((CumT-input_data$T[DataIntervalStart-1]), FC)), use.names=FALSE)
+    } else {
+      FT <- c(unlist(failureC_to_failureT(CumT, FC)), use.names=FALSE)
     }
-    FT <-failureC_to_failureT(T,FC)
-    IF <- failureT_to_interF(failure_T = FT)
+    IF <- c(unlist(failureT_to_interF(failure_T = FT)), use.names=FALSE)
   }
   
   if(input$trendPlotChoice=="LP") {
     plot_data <- laplace_trend_test(IF)
+    if((length(grep("FT",names(input_data)))>0) || (length(grep("IF",names(input_data))) > 0)) {
+      plot_data$Index <- plot_data$Index + (DataIntervalStart - 1)
+    } else {
+      if(DataIntervalStart > 1) {
+        plot_data$Index <- plot_data$Index + input_data$CFC[DataIntervalStart - 1]
+      }
+    }
     q <- q + xlab("Failure Number")+ylab("Laplace Test Statistic")
     q <- q+ggtitle(paste(c("Laplace trend test of"),data_set))
     q <- q + geom_hline(aes(yintercept=c(qnorm(0.1),qnorm(0.05),qnorm(0.01),qnorm(0.001),qnorm(0.0000001),qnorm(0.0000000001)),color=c("0.1","0.05","0.01","0.001","0.0000001","0.0000000001"),linetype="dotted"),alpha=0.8)
     q <- q+xlab("Failure Number")+ylab("Laplace Test Statistic")
   } else if(input$trendPlotChoice=="RA") {
     plot_data <- running_average_test(IF)
+    if((length(grep("FT",names(input_data)))>0) || (length(grep("IF",names(input_data))) > 0)) {
+      plot_data$Index <- plot_data$Index + (DataIntervalStart - 1)
+    } else {
+      if(DataIntervalStart > 1) {
+        plot_data$Index <- plot_data$Index + input_data$CFC[DataIntervalStart - 1]
+      }
+    }
     q <- q + xlab("Failure Number")+ylab("Running Average of Interfailure Times")
     q <- q+ggtitle(paste(c("Running Average trend test of"),data_set))
   }
