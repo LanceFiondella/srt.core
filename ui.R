6cf4c9a1dfaea9f036a98b61cd1de7bce7263140
Allen Nikora <Allen.P.Nikora@jpl.nasa.gov>
2015-08-11 16:38:32 -0700

    Control of the data and trend plot is now in server.R instead of ui.R.

--- ui.R
+++ ui.R
@@ -91,7 +91,7 @@ shinyUI(navbarPage("Software Reliability Assessment in R",
                               
                               mainPanel(
                                 tabsetPanel(
-                                  tabPanel("Plot", textOutput("InputFileError"), textOutput("DataSubsetError"), plotOutput("DataAndTrendPlot",height="700px")), 
+                                  tabPanel("Plot", textOutput("InputFileError"), textOutput("DataSubsetError"), plotOutput("DataAndTrendPlot")), 
                                   tabPanel("Data and Trend Test Table", dataTableOutput("dataAndTrendTable")),
                                   id="DataPlotAndTableTabset"),
                                 width=8
