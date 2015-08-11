5cb75fed0c51213147bbd2af79565a5d30be6fa7
arhik <kkatipally@umassd.edu>
2015-08-11 08:12:22 -0400

    Reverting back to inital fourth tab

--- server.R
+++ server.R
@@ -345,7 +345,7 @@ shinyServer(function(input, output, clientData, session) {#reactive shiny functi
       
       if(length(input$modelResultChoice)>0){
           
-         print("Outer Zone entered")
+         
 
         # Plot initializations
 
@@ -358,33 +358,91 @@ shinyServer(function(input, output, clientData, session) {#reactive shiny functi
             if(i=="Jelinski-Moranda"){
 
             if(length(grep("IF",names(data)))){
+
+
+               # if(input$modelPlotChoice==2){
+               #  #p1 <- ggplot(,aes_string(x=Time,y=Failure));
+               #  new_params <- JM_BM_MLE(IF)
+               #  print(new_params)
+               #  print(typeof(new_params))
+               #  data <- data.frame("FT"=FT,"IF"=IF,"FN"=1:length(FT))
+               #  if(typeof(new_params)=="double"){
+               #    #print("I am 1")
+                  
+               #    frame_params <- data.frame("N0"=c(new_params[1]),"Phi"=c(new_params[2]))
+               #    #print(" I am 2")
+               #    mvf_plot_data <- JM_MVF(frame_params,data)
+               #    #print("I am 3")
+               #    if(input$ModelDataPlotType==1){
+               #      p1 <- p1 + geom_point(data=mvf_plot_data,aes(Time,Failure))+ geom_line(data=mvf_plot_data)# + ggtitle(paste(c("Laplace trend of "),data_set))
+               #    }
+               #    if(input$ModelDataPlotType==2){
+               #      p1 <- p1 + geom_point(data=mvf_plot_data,aes(Time,Failure))
+               #    }
+               #    if(input$ModelDataPlotType==3){
+               #      p1 <- p1 + geom_line(data=mvf_plot_data,aes(Time,Failure))
+
+               #    } 
+               #    if(input$checkboxDataOnPlot){
+               #    original_data <- data.frame("Time"=data$FT,"Failure" =data$FN)
+               #    p1 <- p1 + geom_line(data=original_data,aes(Time,Failure));
+               #  }
+               #  }
+               #  else if(new_params=="nonconvergence"){
+
+               #    #print("I am here so :")
+               #    original_data <- data.frame("Time"=data$FT,"Failure" =data$FN)
+               #    p1 <- p1 + geom_point(data=original_data,aes(Time,Failure))
+               #    #c + geom_text(data = NULL, x = 5, y = 30, label = "plot mpg vs. wt")
+               #    p1 + annotate("segment", x = 0, xend = length(original_data$Failure)/2, y = 0, yend = length(original_data$Time)/2,  colour = "red")
+               #    #p1 <- p1 + annotate("rect", xmin = length(original_data$Failure)/2 -50, xmax = length(original_data$Failure)/2 +50, ymin = length(original_data$Time)/2 -30, ymax = length(original_data$Time)/2 -30, alpha = .2)
+               #    p1 <- p1+ annotate("text", label = "Non-Convergence", x = length(original_data$Failure)/2, y = length(original_data$Time)/2, size = 8, colour = "red")
+               #  }
+               #  else{
+               #    print (" I am an else ")
+               #  }
+                
+               #  p1 <- p1+ggtitle(paste(c("Mean Value function plot of"),input$dataSheetChoice))  
+
+
+
+
+
+
               
               if(input$modelPlotChoice==2){
-                 X_label <- "Time"
-                  Y_label <- "Failure"
+                 
                 #p1 <- ggplot(,ae_string(x=Time,y=Failure));
                 new_params <- JM_BM_MLE(data$IF)
                 data <- data.frame("FT"=data$FT,"IF"=data$IF,"FN"=1:length(data$FT))
-                frame_params <- data.frame("N0"=c(new_params[1]),"Phi"=c(new_params[2]))
-                mvf_plot_data <- JM_MVF(frame_params,data)
-                if(input$ModelDataPlotType==1){
-                  p1 <- p1 + geom_point(data=mvf_plot_data,aes(Time,Failure,color="lines and dots"))+ geom_line(data=mvf_plot_data)# + ggtitle(paste(c("Laplace trend of "),data_set))
+                if(typeof(new_params)=="double"){
+                  frame_params <- data.frame("N0"=c(new_params[1]),"Phi"=c(new_params[2]))
+                  mvf_plot_data <- JM_MVF(frame_params,data)
+                  if(input$ModelDataPlotType==1){
+                    p1 <- p1 + geom_point(data=mvf_plot_data,aes(Time,Failure,color="lines and dots"))+ geom_line(data=mvf_plot_data)# + ggtitle(paste(c("Laplace trend of "),data_set))
+                  }
+                  if(input$ModelDataPlotType==2){
+                    p1 <- p1 + geom_point(data = mvf_plot_data, aes(Time,Failure, color="dots"))
+                  }
+                  if(input$ModelDataPlotType==3){
+                    p1 <- p1 + geom_line(data = mvf_plot_data, aes(Time, Failure, color="lines"))
+                  }
+                  if(input$checkboxDataOnPlot){
+                    original_data <- data.frame("Time" = data$FT, "Failure" = data$FN)
+                    p1 <- p1 + geom_line(data = original_data,aes( Time, Failure, color = "Original Data"))
+                  }
+                  p1 <- p1 + ggtitle(paste(c("Mean Value function plot of"), input$dataSheetChoice))#+ geomline(data=plot_data)
+                  p1 <- p1 + theme(legend.position = c(0.1, 0.9));
+                  p1 <- p1 + scale_color_manual(name = "JM", labels = c("MVF","Original Data"),values = c("blue","red"))
+                  #q <- q + p
                 }
-                if(input$ModelDataPlotType==2){
-                  p1 <- p1 + geom_point(data = mvf_plot_data, aes(Time,Failure, color="dots"))
+                else if(new_params=="nonconvergence"){
+                  original_data <- data.frame("Time"=data$FT,"Failure" =data$FN)
+                  p1 <- p1 + geom_point(data=original_data,aes(Time,Failure))
+                  #p1 + annotate("segment", x = 0, xend = length(original_data$Failure)/2, y = 0, yend = length(original_data$Time)/2,  colour = "red")
+                  p1 <- p1+ annotate("text", label = "Non-Convergence", x = length(original_data$Failure)/2, y = length(original_data$Time)/2, size = 8, colour = "red")
                 }
-                if(input$ModelDataPlotType==3){
-                  p1 <- p1 + geom_line(data = mvf_plot_data, aes(Time, Failure, color="lines"))
-                }
-                if(input$checkboxDataOnPlot){
-                  original_data <- data.frame("Time" = data$FT, "Failure" = data$FN)
-                  p1 <- p1 + geom_line(data = original_data,aes( Time, Failure, color = "Original Data"))
-                }
-                p1 <- p1 + ggtitle(paste(c("Mean Value function plot of"), input$dataSheetChoice))#+ geomline(data=plot_data)
-                p1 <- p1 + theme(legend.position = c(0.1, 0.9));
-                p1 <- p1 + scale_color_manual(name = "JM", labels = c("MVF","Original Data"),values = c("blue","red"))
-                #q <- q + p
-              }
+            }
 
               if(input$modelPlotChoice==1){
                
@@ -568,12 +626,12 @@ shinyServer(function(input, output, clientData, session) {#reactive shiny functi
                 print(typeof(new_params))
                 data <- data.frame("FT"=FT,"IF"=IF,"FN"=1:length(FT))
                 if(typeof(new_params)=="double"){
-                  print("I am 1")
+                  #print("I am 1")
                   
                   frame_params <- data.frame("N0"=c(new_params[1]),"Phi"=c(new_params[2]))
-                  print(" I am 2")
+                  #print(" I am 2")
                   mvf_plot_data <- JM_MVF(frame_params,data)
-                  print("I am 3")
+                  #print("I am 3")
                   if(input$ModelDataPlotType==1){
                     p1 <- p1 + geom_point(data=mvf_plot_data,aes(Time,Failure))+ geom_line(data=mvf_plot_data)# + ggtitle(paste(c("Laplace trend of "),data_set))
                   }
@@ -591,7 +649,7 @@ shinyServer(function(input, output, clientData, session) {#reactive shiny functi
                 }
                 else if(new_params=="nonconvergence"){
 
-                  print("I am here so :")
+                  #print("I am here so :")
                   original_data <- data.frame("Time"=data$FT,"Failure" =data$FN)
                   p1 <- p1 + geom_point(data=original_data,aes(Time,Failure))
                   #c + geom_text(data = NULL, x = 5, y = 30, label = "plot mpg vs. wt")
@@ -741,7 +799,7 @@ shinyServer(function(input, output, clientData, session) {#reactive shiny functi
                 if(input$ModelDataPlotType==3){
                   p1 <- p1+ geom_line(data=mvf_plot_data,aes(Time,Failure))
                 }
-                # if(input$checkboxDataOnPlot){
+                # if(input$checkboxDataOnPlot){  
                 #   original_data <- data.frame("Time"=data$FT,"Failure" =data$FN)
                 #   p <- p + geom_line(data=original_data,aes(Time,Failure));
                 # }
