bbc1c1c7a58cf3985c86cc183ad15d601d30930f
arhik <kkatipally@umassd.edu>
2015-09-14 11:39:45 -0400

    R,MTTF,FaultsRemaining,lnl functions added

--- JM_BM.R
+++ JM_BM.R
@@ -176,7 +176,43 @@ JM_MVF_r <- function(param,d){
   r
 }
 
-JM_lnl  <- function(){
-  t <- -aMLE*(1-exp(-bMLE*tn))+n*log(aMLE)+n*log(bMLE)-bMLE*sum(x)
-  t
-}
+
+
+
+
+ JM_BM_lnl <- function(x,N0_MLE,phi){ 
+    n <- length(x)          
+    secondTerm=0
+    thirdTerm = 0
+
+    for(i in 1:n){
+        secondTerm = secondTerm +log((N0_MLE-(i-1)))
+        thirdTerm = thirdTerm +((N0_MLE-(i-1))*x[i])#x=interFail
+      }
+      llf <- n*log(phi)+ secondTerm-(phi*thirdTerm)
+      return(llf)
+  }
+ 
+ #Faults Remaining
+ 
+ JM_BM_FaultsRemaining <- function(N0_MLE,n){
+  return(floor(N0_MLE-n))
+ }
+ 
+ #Reliability
+   
+ JM_BM_Reliability <- function(n,x,N0_MLE,phi){
+  Reliability <- numeric(0)
+  Reliability <- exp(-phi*(N0_MLE-(i-1))*x[i])
+  return(Reliability)
+ }
+ 
+ #MTTF
+ 
+ JM_BM_MTTF <- function(n,N0_MLE,phi){
+  MTTF=0
+  for(i in 1:n){
+    MTTF = MTTF +(1/(phi*(N0_MLE-(n+(i-1)))))
+  }
+  return(MTTF)
+ }
