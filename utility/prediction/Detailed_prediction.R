e0672771f677252dcdc48c85a2318e67156345ef
arhik <kkatipally@umassd.edu>
2015-10-29 23:21:01 -0400

    removed comments

--- utility/prediction/Detailed_prediction.R
+++ utility/prediction/Detailed_prediction.R
@@ -38,26 +38,7 @@ get_prediction_t <- function(model, params, faults, tn, n){
 		for(i in 1: faults){
 			est_faults <- get(paste(model,"MVF_cont",sep="_"))(params,tn)
 
-			#print("Estimated_faults")
-			#print(est_faults)
-			# if(est_faults - n >= i){
 				tn_n <- try(est_t(model,params,tn,i),silent=TRUE)
-				#print("_")
-				#print("_")
-				#print("_")
-				#print("_")
-				#print("_")
-				#print("_")
-				#print("_")
-				
-				#print(tn_n)
-
-				#print("_")
-				#print("_")
-				#print("_")
-				#print("_")
-				#print("_")
-				#print("_")
 				
 				if((typeof(tn_n)=="double") && (tn_n >= 0)){
 					t <- tn_n - tn
@@ -111,10 +92,7 @@ get_reliability_t <- function(model, params, targetRel, missionTime, tn, numFail
 
 est_t <- function(model,params,tn,steps){
 	est_faults <- get(paste(model,"MVF_cont",sep="_"))(params,tn) # ? ----> Should use floor or not
-	#print("Estimated Faults:")
-	#print(est_faults)
-	#print("")
-	#print("")
+
 	est_time_root <- function(tn){
 		return (get(paste(model,"MVF_cont",sep="_"))(params, tn) -(est_faults+steps))
 	}
