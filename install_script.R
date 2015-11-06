ce22cce4df33d244462dbf17c23327d059484e9f
bdavs <rdavis2@umassd.edu>
2015-11-06 14:32:03 -0500

    update install script to include knitr

--- install_script.R
+++ install_script.R
@@ -6,7 +6,7 @@ if (version$major < 3 || (version$major == 3 && version$minor < 2.2)){ #checks i
   #update R
   if(.Platform$OS.type == "windows"){#automatic update for windows users
     if(!require(installr)) {
-      install.packages("installr"); require(installr)} #load / install+load installr
+      install.packages("installr"); library(installr)} #load / install+load installr
     
     # using the package:
     updateR() # this will start the updating process of your R installation.  It will check for newer versions, and if one is available, will guide you through the decisions you'd need to make.
@@ -19,8 +19,9 @@ if (version$major < 3 || (version$major == 3 && version$minor < 2.2)){ #checks i
 }
 
 #checks for each dependancy and loads or installs then loads
-if(!require(shiny)) {install.packages("shiny");require(shiny)} #shiny is the main interface package
-if(!require(DT)) {install.packages("DT");require(DT)} #DT
-if(!require(gdata)) {install.packages("gdata");require(gdata)} #gdata is used for manipulating the data out of an excel sheet
-if(!require(ggplot2)) {install.packages("ggplot2");require(ggplot2)} #ggplot2 is used for crerating various plots
-if(!require(rootSolve)) {install.packages("rootSolve");require(rootSolve)} #rootSolve is used for finding a root of an equation
+if(!require(shiny)) {install.packages("shiny");library(shiny)} #shiny is the main interface package
+if(!require(DT)) {install.packages("DT");library(DT)} #DT
+if(!require(knitr)) {install.packages("knitr");library(knitr)} #DT
+if(!require(gdata)) {install.packages("gdata");library(gdata)} #gdata is used for manipulating the data out of an excel sheet
+if(!require(ggplot2)) {install.packages("ggplot2");library(ggplot2)} #ggplot2 is used for crerating various plots
+if(!require(rootSolve)) {install.packages("rootSolve");library(rootSolve)} #rootSolve is used for finding a root of an equation
