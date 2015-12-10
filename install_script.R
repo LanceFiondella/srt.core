330e4f4e3f0ec9c9ed6400d293058c75ffa1b8ed
Daniel Garcia <Dgarcia1@umassd.edu>
2015-12-10 18:20:33 -0500

    Update install_script.R

--- install_script.R
+++ install_script.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
-#if(!require(testthat)) {
-#	install.packages("testthat", repos="http://cran.rstudio.com/")
+if(!require(testthat)) {
+	install.packages("testthat", repos="http://cran.rstudio.com/")
 	library("testthat")
-#} #DT
+} #DT
 
