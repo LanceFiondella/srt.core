4f2ae3294dc078bcd2ba57787ebeb047c9b7f90b
bdavs <rdavis2@umassd.edu>
2015-12-11 00:39:27 -0500

    added some test cases

--- run_tests.R
+++ run_tests.R
@@ -1,6 +1,7 @@
 library('testthat')
 
 source('sample.R')
+source('tests/test_functions.R')
 
 test_dir('tests', reporter='Summary')
 
--- tests/test.1.R
+++ tests/test.1.R
@@ -1,11 +1,13 @@
-expect_that(1 ^ 1, equals(1))
-expect_that(2 ^ 2, equals(2))
- 
-expect_that(2 + 2 == 4, is_true())
-expect_that(2 == 1, is_false())
- 
-expect_that(1, is_a('numeric'))
- 
+expect_that(is_valid_input(JM_input),is_true())
+expect_that(is_valid_input(GM_input),is_true())
+expect_that(is_valid_input(GO_input),is_true())
+expect_that(is_valid_input(DSS_input),is_true())
+expect_that(is_valid_input(Wei_input),is_true())
+
+
+
+#expect_that(is_valid_result())
+
 expect_that(print('Hello World!'), prints_text('Hello World!'))
  
 expect_that(log('a'), throws_error())
