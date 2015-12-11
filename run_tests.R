6f28abecdad1758cf6f0e566c244d9bef782098e
bdavs <rdavis2@umassd.edu>
2015-12-10 19:54:50 -0500

    dumb stuff

--- run_tests.R
+++ run_tests.R
@@ -2,7 +2,7 @@ library('testthat')
 
 source('sample.R')
 source('tests/test_functions.R')
-source('models/Model_specifications')
+source('models/Model_specifications.R')
 
 test_dir('tests', reporter='Summary')
 
