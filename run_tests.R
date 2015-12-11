2c4ca2df3fcd9292e6d767526cb0882bbff2f9d7
Dan Garcia <dgarcia1@umassd.edu>
2015-12-11 18:31:04 -0500

    run_test added directory loop

--- run_tests.R
+++ run_tests.R
@@ -2,8 +2,17 @@ library('testthat')
 
 source('sample.R')
 source('tests/needed.R')
-source('models/Model_specifications.R')
-#source("test.1.R")
 
-test_dir('tests', reporter='Summary')
+model_directories <- list.dirs("./models")
+model_directories <- tail(model_directories,-1)
+print(model_directories)
+for(directory in model_directories){
+	print(directory)
+	model_files <- list.files(directory)
+	for(file in model_files){
+		current_file <- paste(directory,file,sep="/")
+		print(current_file)
+		source(current_file)
+	}
+}
 
