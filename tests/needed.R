a877de907b5e7b67f5ce020a8304b3a82ddf8516
Daniel Garcia <Dgarcia1@umassd.edu>
2015-12-11 17:22:52 -0500

    Added breaking statements

--- tests/needed.R
+++ tests/needed.R
@@ -2,9 +2,11 @@ needed_Bool <- function(obj){
 	ret <- tryCatch(is.logical(obj),
 			warning = function(w){
 				print(w)
+				quit("no",status = -1)
 			},
 			error = function(e){
 				print(e)
+				quit("no",status = -1)
 			}
 			)
 	return(ret)
