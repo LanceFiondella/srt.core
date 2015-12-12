5f70f062db675cbf67fdab1ad5ce9564839cdbf6
Dan Garcia <dgarcia1@umassd.edu>
2015-12-11 19:28:47 -0500

    added stuff

--- run_tests.R
+++ run_tests.R
@@ -1,18 +1,23 @@
 library('testthat')
-
+GM_input <<- NULL
 source('sample.R')
 source('tests/needed.R')
 
-model_directories <- list.dirs("./models")
-model_directories <- tail(model_directories,-1)
+model_directories <<- dir("./models", no..=TRUE)
 print(model_directories)
 for(directory in model_directories){
-	print(directory)
-	model_files <- list.files(directory)
+	current_directory <<- paste(".","models",directory,sep="/")
+	print(current_directory)
+	model_files <<- list.files(current_directory)
 	for(file in model_files){
-		current_file <- paste(directory,file,sep="/")
+		current_file <<- paste(current_directory,file,sep="/")
 		print(current_file)
-		source(current_file)
+		Sys.setenv("e" = source(current_file))
+		if(length(grep("Model_specifications.R",file))){
+  		current_model_input <<- paste(directory,"_input",sep="")
+  		print(get(current_model_input))
 	}
+	}
+	
 }
 
