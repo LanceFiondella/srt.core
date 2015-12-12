d6af2c8970363f8f5d125eb438e45b0ca181ec59
Robert Davis <rdavis2@umassd.edu>
2015-12-11 19:47:38 -0500

    Update install_script.R

--- install_script.R
+++ install_script.R
@@ -12,7 +12,7 @@ if (version$major < 3 || (version$major == 3 && version$minor < 2.2)){ #checks i
   #update R
   if(.Platform$OS.type == "windows"){#automatic update for windows users
     if(!require(installr)) {
-      install.packages("installr"); library(installr)} #load / install+load installr
+      install.packages("installr", repos="http://cran.rstudio.com/"); library(installr)} #load / install+load installr
     
     # using the package:
     updateR() # this will start the updating process of your R installation.  It will check for newer versions, and if one is available, will guide you through the decisions you'd need to make.
@@ -25,9 +25,9 @@ if (version$major < 3 || (version$major == 3 && version$minor < 2.2)){ #checks i
 }
 
 #checks for each dependancy and loads or installs then loads
-if(!require(shiny)) {install.packages("shiny");library(shiny)} #shiny is the main interface package
-if(!require(DT)) {install.packages("DT");library(DT)} #DT
-if(!require(knitr)) {install.packages("knitr");library(knitr)} #DT
-if(!require(gdata)) {install.packages("gdata");library(gdata)} #gdata is used for manipulating the data out of an excel sheet
-if(!require(ggplot2)) {install.packages("ggplot2");library(ggplot2)} #ggplot2 is used for crerating various plots
-if(!require(rootSolve)) {install.packages("rootSolve");library(rootSolve)} #rootSolve is used for finding a root of an equation
+if(!require(shiny)) {install.packages("shiny", repos="http://cran.rstudio.com/");library(shiny)} #shiny is the main interface package
+if(!require(DT)) {install.packages("DT", repos="http://cran.rstudio.com/");library(DT)} #DT
+if(!require(knitr)) {install.packages("knitr", repos="http://cran.rstudio.com/");library(knitr)} #DT
+if(!require(gdata)) {install.packages("gdata", repos="http://cran.rstudio.com/");library(gdata)} #gdata is used for manipulating the data out of an excel sheet
+if(!require(ggplot2)) {install.packages("ggplot2", repos="http://cran.rstudio.com/");library(ggplot2)} #ggplot2 is used for crerating various plots
+if(!require(rootSolve)) {install.packages("rootSolve", repos="http://cran.rstudio.com/");library(rootSolve)} #rootSolve is used for finding a root of an equation
