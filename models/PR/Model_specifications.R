2846d5a903c84baec09ba7c4a5efa8b5261c1359
Mihir Kavatkar <mkavatkar@umassd.edu>
2015-12-14 17:44:58 -0500

    Create Model_specifications.R
    Model specification for Pareto Model

---
+++ models/PR/Model_specifications.R
@@ -0,0 +1,11 @@
+# Pareto Model
+PR_input <- c("FT")
+PR_methods <- c("ECM")
+PR_params <- c("aMLE","bMLE","cMLE")
+PR_numfailsparm <- c(1)
+PR_fullname <- c("PARETO")
+PR_plotcolor <- c("red")
+PR_type <- c("FR","Exp")
+PR_Finite <- TRUE
+PR_Results<- data.frame("format"="xlsx","fileName"="model_data") # Sheet name is generated in test script, based on method.
+# JM_Results<- data.frame("format"="xlsx","fileName"="model_data","sheet"="PR")
