ef5cdd8dd27ab912216bdb5fc2de3239d754d7fd
saikath1 <saikath.bhattacharya@gmail.com>
2015-12-15 15:50:22 -0500

    name changed to LNORM

--- models/Lnorm/Model_specifications.R
+++
@@ -1,9 +0,0 @@
-#Lognormal model
-Lnorm_input <- c("FT")
-Lnorm_methods <- c("ECM")
-Lnorm_params <- c("a","mu","sigma")
-Lnorm_type <- c("Exp")
-Lnorm_numfailsparm <- c(1)
-Lnorm_fullname <- c("Lognormal")
-Lnorm_plotcolor <- c("orange")
-Lnorm_Finite <- FALSE
