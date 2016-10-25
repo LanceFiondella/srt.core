8313273421b9240ae91a880d83333f5e93de496f
Shekar <gv.shekar@gmail.com>
2016-10-25 16:30:10 -0400

    Removed zzzz model and tracker from ui

--- ui.R
+++ ui.R
@@ -447,7 +447,7 @@ shinyUI(navbarPage("Software Reliability Assessment in R",
                                 )                              
                               )
                             )
-                   ),
-tags$footer(includeHTML("analytics/clustrmaps.html")),
-tags$footer(includeHTML("analytics/statcounter.html"))
+                   )
+#tags$footer(includeHTML("analytics/clustrmaps.html")),
+#tags$footer(includeHTML("analytics/statcounter.html"))
 ))
