90d782336607038a0eaadc59dd72db8de7787b97
shekar <gv.shekar@gmail.com>
2016-12-30 21:31:37 -0500

    Cherry picked branch 576812b from develop and changed plot fonts to 20

--- utility/plots/Plot_Trend_Tests.R
+++ utility/plots/Plot_Trend_Tests.R
@@ -70,7 +70,7 @@ plot_trend_tests <- function(in_data, convertedFCData, DataName, DataRange, Tren
     if(PlotType=="lines"){
       localTrendPlot <- localTrendPlot + geom_step(data=plot_data,aes(index,trend_test_statistic))
     }
-    localTrendPlot <- localTrendPlot + theme(legend.position = "bottom", text = element_text(size=25))
+    localTrendPlot <- localTrendPlot + theme(legend.position = "bottom", text = element_text(size=20))
   } else {
     PlotFault <- TRUE
   }
