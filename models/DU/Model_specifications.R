2b873c7fa75ea711ecc408bf7169e4ed2464aa77
Ross Raymond <Ross.Raymond@aero.org>
2018-01-12 14:20:55 -0800

    Used the online GitHub to upload the models--apparently it uploaded them to the main folder despite showing the 'models' folder. This commit is to move the new models into the correct folder.

---
+++ models/DU/Model_specifications.R
@@ -0,0 +1,10 @@
+# Duane model
+DU_input <- c("FT")
+DU_methods <- c("LS", "SS")  # BM TBD
+DU_params <- c("alpha", "beta")
+DU_type <- c("NHPP", "Power")
+DU_numfailsparm <- NA
+DU_fullname <- c("Duane")
+DU_plotcolor <- c("darkgoldenrod")
+DU_Finite <- FALSE
+DU_prefer_method <- c("LS")
---
+++ models/MO/Model_specifications.R
@@ -0,0 +1,10 @@
+# Musa-Okumoto model
+MO_input <- c("FT")
+MO_methods <- c("ECM", "BM") # Optimization methods are attempted in order of listing
+MO_params <- c("theta0","theta1")
+MO_type <- c("NHPP", "Log")
+MO_numfailsparm <- c(1)
+MO_fullname <- c("Musa-Okumoto")
+MO_plotcolor <- c("purple")
+MO_Finite <- FALSE
+MO_prefer_method <- c("ECM")
