844773862200458ea2d91e040408b50345082e9d
Shekar V <vshekar@umassd.edu>
2018-04-02 16:53:01 -0400

    Opening dictionary file with UTF-8 encoding

--- utility/UIsettings.R
+++ utility/UIsettings.R
@@ -1,5 +1,5 @@
 language <<- "jp"
-dictionary <<- read.csv("dictionary.csv")
+dictionary <<- read.csv("dictionary.csv", encoding = 'UTF-8')
     rownames(dictionary) <- dictionary$variable
 translate <- function(text){
     toString(dictionary[text, language])
